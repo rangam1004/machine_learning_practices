f77d93a4366767104e65bdeb2e928f973d338e30
Mohanarangam <rangam1004@gmail.com>
2020-09-27 22:07:58 +0530

    Classifying Iris Species update

--- practices/matplotlip_example.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Generate a sequence of numbers from -10 to 10 with 100 steps in between
-x = np.linspace(-10, 10, 100)
-# Create a second array using sine
-y = np.sin(x)
-# The plot function makes a line chart of one array against another
-plt.plot(x, y, marker="x")
